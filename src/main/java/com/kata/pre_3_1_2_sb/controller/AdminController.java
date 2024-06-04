8a14aad359b7cdaa66830111dbb6c5ad29fcdf9c
Sergei Ivchenko <ivcsgi@gmail.com>
2024-06-04 18:46:20 +0600

    Pre 3.3 Work on comments

--- src/main/java/com/kata/pre_3_1_2_sb/controller/AdminController.java
+++ src/main/java/com/kata/pre_3_1_2_sb/controller/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +17,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
@@ -30,7 +30,6 @@ public class AdminController {
 
     @PostMapping("/new")
     public ResponseEntity<HttpStatus> saveUser(@RequestBody UserRequest userRequest) {
-        System.out.println(userRequest);
         userService.save(userRequest);
         return ResponseEntity.ok(HttpStatus.OK);
     }
@@ -52,7 +51,6 @@ public class AdminController {
 
     @PutMapping("/edit")
     public ResponseEntity<UserRequest> updateUser(@RequestBody UserRequest userRequest) {
-        System.out.println("User: " + userRequest);
         userService.updateUser(userRequest);
         return ResponseEntity.ok(userRequest);
     }
